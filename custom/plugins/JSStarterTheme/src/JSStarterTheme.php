d9bc46fa65a3780066349f643134aed730253639
Joel Storr <joel.storr@ewe.net>
2023-03-22 08:56:59 +0100

    first commit

---
+++ custom/plugins/JSStarterTheme/src/JSStarterTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace JSStarterTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class JSStarterTheme extends Plugin implements ThemeInterface
+{
+}
